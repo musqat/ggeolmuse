dcc4282982c32d73a83c458bfdb119284afe9352
musqat <hjs90561@naver.com>
2025-08-14 01:25:09 +0900

    init skeleton (modules: bom, market-data, user, pricing, backtest)

---
+++ src/main/java/com/muscat/ggeolmuse/GgeolmuseApplication.java
@@ -0,0 +1,13 @@
+package com.muscat.ggeolmuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgeolmuseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GgeolmuseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/muscat/ggeolmuse/GgeolmuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscat.ggeolmuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgeolmuseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
