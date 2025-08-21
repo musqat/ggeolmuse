1d71d189dc803b120ef8e474425ab0c679a8afbf
musqat <hjs90561@naver.com>
2025-08-21 22:13:14 +0900

    Test

--- user/src/test/java/com/muscat/user/UserApplicationTests.java
+++ user/src/test/java/com/muscat/user/UserApplicationTests.java
@@ -2,12 +2,12 @@ package com.muscat.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
